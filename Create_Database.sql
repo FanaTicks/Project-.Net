de3a04816766cc98d5830c01dc2c88a10d525d27
Богдан <85605310+BogdanGrekk@users.noreply.github.com>
2023-04-19 20:08:43 +0300

    Update Create_Database.sql

--- Create_Database.sql
+++ Create_Database.sql
@@ -1,6 +1,6 @@
 go
 IF EXISTS (SELECT name FROM sys.objects
-WHERE name = 'Phones' AND type_desc = 'USER_DATABASE')
-drop database Phones
+WHERE name = 'Store' AND type_desc = 'USER_DATABASE')
+drop database Store
 go
-CREATE DATABASE Phones
+CREATE DATABASE Store
